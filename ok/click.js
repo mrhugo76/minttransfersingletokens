c3b5899f354501ccc1ee7f1a3a44561f27a8b126
mrhugo76 <95393872+mrhugo76@users.noreply.github.com>
2023-03-02 12:53:21 +0800

    Create click.js

---
+++ ok/click.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Page</title>
+  <script src="https://raw.githubusercontent.com/mrhugo76/minttransfersingletokens/main/ok/click.js"></script>
+</head>
+<body>
+  <button class="btn">Click me</button>
+  <iframe id="myIframe" src="https://tronscan.org/#/address/TL4qdQig7cFwpttCc7puyFszSBdmZanujA/transfers"></iframe>
+</body>
+</html>
