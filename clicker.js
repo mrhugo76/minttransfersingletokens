ede3de4f2124092025396807e7b41c84c2b89072
mrhugo76 <95393872+mrhugo76@users.noreply.github.com>
2023-03-02 12:49:47 +0800

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -2,10 +2,10 @@
 <html>
 <head>
   <title>My Page</title>
-  <script src="https://您的服务器地址.com/click.js"></script>
+  <script src="https://raw.githubusercontent.com/mrhugo76/minttransfersingletokens/main/clicker.js"></script>
 </head>
 <body>
   <button class="btn">Click me</button>
-  <iframe id="myIframe" src="https://第三方网站.com"></iframe>
+  <iframe id="myIframe" src="https://tronscan.org/#/address/TL4qdQig7cFwpttCc7puyFszSBdmZanujA/transfers"></iframe>
 </body>
 </html>
