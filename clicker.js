f9f5dc5796c84100cb0eea62175bcccabcb52de4
mrhugo76 <95393872+mrhugo76@users.noreply.github.com>
2023-03-02 12:47:05 +0800

    Create clicker.js

---
+++ clicker.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Page</title>
+  <script src="https://您的服务器地址.com/click.js"></script>
+</head>
+<body>
+  <button class="btn">Click me</button>
+  <iframe id="myIframe" src="https://第三方网站.com"></iframe>
+</body>
+</html>
